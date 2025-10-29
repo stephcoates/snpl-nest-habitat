0d360ec8fdb45000483f6911a3753099430002b5
Stephanie Coates <scoates3@gmail.com>
2025-10-28 21:23:48 -0700

    added Pearson's correlation test for pioneer functional groups and TwoVeg

--- R/3_glm_preparation.R
+++ R/3_glm_preparation.R
@@ -140,3 +140,9 @@ corr_matrix1.fall <- cor(hab_corr_df1.fall)
 # Secondary Stabilizers and Two, SG
 # Pioneer Stabilizers and Two, DeadVegManual
 # NonVeg and WO
+
+# Calculate correlation between all pioneers (builders and stabilizers) and TwoVeg
+all_df <- all_df %>% 
+  mutate(pioneers = Pioneer_Builders + Pioneer_Stabilizers)
+cor.test(all_df2$pioneers, all_df2$TwoVeg)
+
